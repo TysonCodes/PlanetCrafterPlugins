aee21804d8ac586af00890d9ed48f5c3e7ae0cd6
Tyson Marchuk <TysonCodes@gmail.com>
2022-04-02 10:31:36 -0400

    Add empty teleporters plugin.

---
+++ Teleporters/Plugin.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using BepInEx;
+using BepInEx.Configuration;
+using HarmonyLib;
+using MijuTools;
+using SpaceCraft;
+using UnityEngine.InputSystem;
+
+namespace Teleporters_Plugin
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Planet Crafter.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        private void Awake()
+        {
+            harmony.PatchAll(typeof(Teleporters_Plugin.Plugin));
+
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+                
+        private void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+    
+}
