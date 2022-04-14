716609a3715dcfd208b4ba0d92f33da13f6f4c51
Tyson Marchuk <TysonCodes@gmail.com>
2022-04-14 08:29:45 -0400

    Add ability to index existing group data and terraform stage. Terraform stages should be moved somewhere it's guaranteed to exist.

--- PluginFramework/Plugin.cs
+++ PluginFramework/Plugin.cs
@@ -2,7 +2,9 @@
 using BepInEx;
 using BepInEx.Logging;
 using HarmonyLib;
+using MijuTools;
 using SpaceCraft;
+using UnityEngine;
 
 namespace PluginFramework
 {
@@ -14,11 +16,61 @@ namespace PluginFramework
 
         private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
 
+        private static List<GroupData> gameGroupData;
+        private static Dictionary<string, GroupData> groupDataById = new Dictionary<string, GroupData>();
+        private static Dictionary<string, TerraformStage> terraformStageById = new Dictionary<string, TerraformStage>();
+        private static Dictionary<string, GameObject> gameObjectById = new Dictionary<string, GameObject>();
+        private static Dictionary<string, Sprite> iconById = new Dictionary<string, Sprite>();
+
         private void Awake()
         {
+            bepInExLogger = Logger;
+
             harmony.PatchAll(typeof(PluginFramework.Framework));
 
             Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        [HarmonyPrefix]
+        [HarmonyPatch(typeof(StaticDataHandler), "LoadStaticData")]
+        private static bool StaticDataHandler_LoadStaticData_Prefix(ref List<GroupData> ___groupsData)
+        {
+            // Save reference to group data.
+            gameGroupData = ___groupsData;
+
+            IndexExistingGroupData();
+            LoadTerraformStages();
+
+            return true;
+        }
+
+        private static void IndexExistingGroupData()
+        {
+            foreach (var groupData in gameGroupData)
+            {
+                groupDataById[groupData.id] = groupData;
+                if (groupData.associatedGameObject)
+                {
+                    gameObjectById[groupData.associatedGameObject.name] = groupData.associatedGameObject;
+                }
+                if (groupData.icon)
+                {
+                    iconById[groupData.icon.name] = groupData.icon;
+                }
+            }
+            bepInExLogger.LogInfo($"Created index of previous group data. Size = {groupDataById.Count}");
+        }
+
+        private static void LoadTerraformStages()
+        {
+            if (Managers.GetManager<TerraformStagesHandler>())
+            {
+                List<TerraformStage> terraformStages = Managers.GetManager<TerraformStagesHandler>().GetAllTerraGlobalStages();
+                foreach (TerraformStage stage in terraformStages)
+                {
+                    terraformStageById[stage.GetTerraId()] = stage;
+                }
+            }
         }
 
         private void OnDestroy()
@@ -26,5 +78,4 @@ namespace PluginFramework
             harmony.UnpatchSelf();
         }
     }
-    
 }
