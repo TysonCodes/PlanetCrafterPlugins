7eff7d4a0b5e253f6e29e22a522badea63b65a1e
Tyson Marchuk <TysonCodes@gmail.com>
2022-04-14 08:17:07 -0400

    Add shell to move framework into.

---
+++ PluginFramework/Plugin.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+using BepInEx;
+using HarmonyLib;
+using SpaceCraft;
+
+namespace PluginFramework
+{
+    [BepInPlugin(PluginInfo.PLUGIN_GUID, PluginInfo.PLUGIN_NAME, PluginInfo.PLUGIN_VERSION)]
+    [BepInProcess("Planet Crafter.exe")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private readonly Harmony harmony = new Harmony(PluginInfo.PLUGIN_GUID);
+
+        private void Awake()
+        {
+            harmony.PatchAll(typeof(PluginFramework.Plugin));
+
+            Logger.LogInfo($"Plugin {PluginInfo.PLUGIN_GUID} is loaded!");
+        }
+
+        private void OnDestroy()
+        {
+            harmony.UnpatchSelf();
+        }
+    }
+    
+}
